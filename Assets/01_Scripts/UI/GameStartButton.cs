45061964a4718e268efe6fd28291ef898d72d97f
treetreet <treetreet@kakao.com>
2026-08-14 00:04:52 +0900

    -

---
+++ Assets/01_Scripts/UI/GameStartButton.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI_Scripts
+{
+    public class GameStartButton : MonoBehaviour
+    {
+        public void GameStart()
+        {
+            SceneManager.LoadScene("Tutorial");
+        }
+    }
+}
